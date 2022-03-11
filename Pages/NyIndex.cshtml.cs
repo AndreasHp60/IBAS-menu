29a132c6fe65e36b2916091e6459e8f15dd5e814
Andreas Clemmen Bisgaard <andreashp60@live.com>
2022-03-11 11:20:58 +0100

    ny side

---
+++ Pages/NyIndex.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IBAS_menu.Pages;
+
+public class NyIndexModel : PageModel
+{
+    private readonly ILogger<NyIndexModel> _logger;
+
+    public NyIndexModel(ILogger<NyIndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
